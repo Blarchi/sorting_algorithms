a5ee3fdb254296579869bcbab6620f60d9e2f63c
Blarchi <you@example.com>
2024-03-19 12:22:07 -0700

    algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,33 @@
-
-#include <stdio.h>
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- *                       using the Insertion Sort algorithm.
- * @list: Pointer to the head of the doubly linked list
- */
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *tmp;
+	int n;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    current = (*list)->next;
-
-    while (current != NULL)
-    {
-        temp = current->next;
-
-        while (current->prev != NULL && current->n < current->prev->n)
-        {
-            current->prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = current->prev;
-
-            current->next = current->prev;
-            current->prev = current->prev->prev;
-
-            if (current->prev != NULL)
-                current->prev->next = current;
-
-            current->next->prev = current;
-
-            if (current->prev == NULL)
-                *list = current;
-
-            print_list(*list);
-        }
-
-        current = temp;
-    }
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
-
